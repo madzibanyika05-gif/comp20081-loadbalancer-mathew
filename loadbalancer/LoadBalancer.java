03e7903735b565211e65a9b36a35dcfdb80f5eab
Mathew <n1192518@ntu.ac.uk>
2026-02-01 17:06:13 +0000

    getting host errors trying to fix load balancer java

--- loadbalancer/LoadBalancer.java
+++ loadbalancer/LoadBalancer.java
@@ -13,8 +13,8 @@ public class LoadBalancer {
 
     // backend storage nodes
     private static final List<InetSocketAddress> BACKENDS = List.of(
-            new InetSocketAddress("storage-a", 9001),
-            new InetSocketAddress("storage-b", 9002)
+            new InetSocketAddress("storage-a", 9101),
+            new InetSocketAddress("storage-b", 9102)
     );
 
     private static final int LISTEN_PORT = 9000;
